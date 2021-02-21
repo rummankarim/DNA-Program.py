a454b14d6648a33a19ddbff28b5b26a97aa5bebc
Rumman Karim <rumman.23197@gmail.com>
2021-02-21 10:53:37 -0500

    update description & docstring

--- DNA Program.py
+++ DNA Program.py
@@ -1,34 +1,32 @@
-#Rumman-Al-Karim
-#This program allows the user to enter a dna string 
+# Rumman-Al-Karim
+# This script allows the user to enter a dna string and 
+# return matching base pairs
+
 def comp_dna(dna):
     """
     A <-> T   &  C <-> G
-
-    Output: [('A', 'T'), ('C', 'G'), etc...]
+    Example Output: [('A', 'T'), ('C', 'G'), etc...]
     """
+    convs = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
 
-    {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'} 
-
-    if type(dna) == list: 
+    if type(dna) == list:
         dna = ''.join(dna)
-    
+
     dna = dna.upper()
-    helix = [] 
-    str2 = '' 
+    helix = []
 
-    for i in dna: 
-        helix.append((i, convs[i])) 
-        str2 += i
+    for i in dna:
+        helix.append((i, convs[i]))
 
-    return helix, str2 
+    return helix
   
   
-dna = [('CTTAGTTC'),('GGGTAACCT')] 
-# [('C', 'G'), ('T', 'A'), ('T', 'A'), ('A', 'T'), ('G', 'C'), ('T', 'A'), ('T', 'A'), ('C', 'G')]
+dna = 'CTTAGTTCGGGTAACCT'
+"""
+Expected Output:
+[('C', 'G'), ('T', 'A'), ('T', 'A'), ('A', 'T'), ('G', 'C'), ('T', 'A'), ('T', 'A'), ('C', 'G')]
+"""
 
-
-
-dna = list(dna)  
-str2, helix = comp_dna(dna)
+dna = list(dna)
+helix = comp_dna(dna)
 print(helix)
-print(str2)
